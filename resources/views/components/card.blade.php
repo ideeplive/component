d8afc9193a9b9121b8c1ae443d315aeb5b4bb2e8
Home <ideeplive@gmail.com>
2021-10-18 21:18:47 +0530

    Today Update

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h2>The Card Title</h2>
+    <h4>The Card SubTitle</h4>
+    <p>Description is Here</p>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,15 @@
 <h1>Component Features</h1>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
+<x-card/>
